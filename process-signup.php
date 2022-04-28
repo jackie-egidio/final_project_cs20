c6d48fbe4b4ecdaedb71586d47c472bac9924ec3
jackie-egidio <52717215+jackie-egidio@users.noreply.github.com>
2022-04-27 21:34:09 -0400

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -1,77 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <style>
-            html, body {
-				margin: 0;
-				padding: 0;
-				font-family: sans-serif;
-			}
-
-			.head {
-				background-color: #DCD0EA;
-			}
-
-			.head-content {
-				padding: 16px;
-			}
-
-			.head-text {
-				font-family: sans-serif;
-				color: white;
-				font-size: 30px;
-				text-align: center; 
-				padding: 20px;
-			}
-
-			.form {
-				width: 50%;
-				margin: auto;
-				padding: 100px;
-			}
-
-			.form-text {
-				font-family: sans-serif;
+        <link rel="stylesheet" href="style.css">
+		<style>
+			.signup-successful {
+				padding: 340px;
 				color: #7D3C98;
-				font-size: 30px;
-				text-align: center; 
-				padding: 20px;
+				text-align: center;
+				font-size: 42px;
 			}
 
-			.footer, .nav {
-				padding: 2px;
+			.signup-successful a {
+				text-decoration: none;
+				color: #DCD0EA;
 			}
 
-			.nav a:hover {
-				color: #F2BFD7;
-			}
-
-			.nav {
+			.signup-successful a:hover {
+				text-decoration: none;
+				color: #DCD0EA;
 				background-color: #7D3C98;
 			}
-
-			.footer {
-				background-color: #DCD0EA;
-			}
-
-			.footer li, .nav li {
-				display: inline;
-			}
-
-			.footer a, .nav a {
-				padding-right: 30px;
-				text-decoration: none;
-			}
-
-			.footer a {
-				color: #7D3C98;
-			}
-
-			.nav a {
-				color: white;
-			}
-        </style>
-    </head>
+		</style>
+	</head>
     <body>
         <div class="head">
 			<div class="head-content">
@@ -80,73 +30,61 @@
 				</div>
 			</div>
 		</div>
-		<div class="nav">
-			<ul>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/index.html">Home</a></li>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/contactus.html">Contact Us</a></li>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/diaryform.html">Diary</a>
-				<li style="float: right"><a href="https://jacquelinee.sgedu.site/final_project/signup.html">Signup</a></li>
-				<li style="float: right"><a href="https://jacquelinee.sgedu.site/final_project/login.html">Login</a></li>
-			</ul>
+		<div class="nav" id="nav">
+			<a href="https://jacquelinee.sgedu.site/final_project/index.html">Home</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/contactus.html">Contact Us</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/signup.html" style="float: right">Signup</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/login.php" style="float: right">Login</a>
 		</div>
-        <?php
-            // SERVER-SIDE VALIDATION
-            // name validation
-            if (empty($_POST['fname']) || empty($_POST['lname'])) {
-                die("Must enter a first and last name");
+		<?php
+			// SERVER-SIDE VALIDATION
+			// name validation
+			if (empty($_POST['fname']) || empty($_POST['lname'])) {
+				die("Must enter a first and last name");
+			}
+
+			// email validation
+			if (! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+				die("Must enter a valid email address");
+			}
+
+			// password validation
+			if (strlen($_POST['pass']) < 8) {
+				die("Must be at least 8 characters");
+			}
+			if ($_POST['pass'] != $_POST['pass_confirm']) {
+				die("Passwords entered do not match");
+			}
+
+			// protects password from being stored raw in the database
+			$password_hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+			// connect to server using contents of database.php file
+			$mysqli = require __DIR__ . "/login_database.php";
+			
+			// POST TO DATABASE
+			$sql = "INSERT INTO login_info (fname, lname, username, password, email)
+					VALUES (?, ?, ?, ?, ?)";
+
+            $stmt = $mysqli->stmt_init();
+
+            if ( ! $stmt->prepare($sql)) {
+                die("SQL error: " . $mysqli->error);
             }
 
-            // email validation
-            if (! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                die("Must enter a valid email address");
-            }
+            $stmt->bind_param("sssss", $_POST['fname'], $_POST['lname'], $_POST['uname'], $password_hash, $_POST['email']);
+            $stmt->execute();
 
-            // password validation
-            if (strlen($_POST['pass']) < 8) {
-                die("Must be at least 8 characters");
-            }
-            if ($_POST['pass'] != $_POST['pass_confirm']) {
-                die("Passwords entered do not match");
-            }
-
-            // protects password from being stored raw in the database
-            $password_hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-
-            // connect to server using contents of database.php file
-            $mysqli = require __DIR__ . "/login_database.php";
-
-            // GET DATA FROM HTML FORM
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $username = $_POST['uname'];
-            $email = $_POST['email'];
-            
-            // POST TO DATABASE
-            $sql = "INSERT INTO login_info (fname, lname, username, password, email)
-                    VALUES (?, ?, ?, ?, ?)";
-
-            // protects against attacks to SQL string
-            $stmt = mysqli_stmt_init($conn);
-
-            if ( ! mysqli_stmt_prepare($stmt, $sql)) {
-                die(mysqli_error($conn));
-            }
-
-            mysqli_stmt_bind_param($stmt, "sssss", $fname, $lname, $uname, $password_hash, $email);
-            mysqli_stmt_execute($stmt);
-
-            header("Location: signup-success.html");
-
-            //close the connection 
-            $conn->close();
-        ?>
+			$conn->close();
+		?>
+		<div class="signup-successful">
+			Signed up successfully! You can now <a href="https://jacquelinee.sgedu.site/final_project/login.php">log in</a>.
+		</div>
         <div class="footer">
-			<ul>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/index.html">Home</a></li>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/contactus.html">Contact Us</a></li>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/signup.html">Signup</a></li>
-				<li><a href="https://jacquelinee.sgedu.site/final_project/login.html">Login</a></li>
-			</ul>
+			<a href="https://jacquelinee.sgedu.site/final_project/index.html">Home</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/contactus.html">Contact Us</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/signup.html">Signup</a>
+			<a href="https://jacquelinee.sgedu.site/final_project/login.php">Login</a>
 		</div>
     </body>
 </html>
