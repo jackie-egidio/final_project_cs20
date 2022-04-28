2c957b09ff15cdf1d5c87e2503fc3bb6f0d21dce
Janya Gambhir <67549506+janya123@users.noreply.github.com>
2022-04-28 01:47:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const green = document.getElementById('green');
+
+green.addEventListener('click', function onClick(event) {
+  const input = document.getElementById('b');
+  const input2 = document.getElementById('c');
+  document.body.style.backgroundColor = 'black';
+  a.style.backgroundColor = 'black';
+  a.style.color = 'white';
+  input.style.backgroundColor = 'black';
+  results.style.color = 'white';
+  input.style.backgroundColor = 'black';
+  input2.style.backgroundColor = 'black';
+  input.style.color = 'white';
+  input2.style.color = 'white';
+});
---
+++ index2.js
@@ -0,0 +1,15 @@
+const red = document.getElementById('red');
+
+red.addEventListener('click', function onClick(event) {
+  const input = document.getElementById('b');
+  const input2 = document.getElementById('c');
+  document.body.style.backgroundColor = 'white';
+  a.style.backgroundColor = 'white';
+  a.style.color = 'black';
+  input.style.backgroundColor = 'white';
+  results.style.color = 'black';
+  input.style.backgroundColor = 'white';
+  input2.style.backgroundColor = 'white';
+  input.style.color = 'black';
+  input2.style.color = 'black';
+});
